dd9fe48fd15080edb75aba2b0481ce1c34a4a45c
Mamoudou iyabano <mamoudouiyabano@gmail.com>
2022-08-08 19:21:50 +0200

    firstcommit

---
+++ src/main/java/com/alc/gestock/GestionDeStockApplication.java
@@ -0,0 +1,13 @@
+package com.alc.gestock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionDeStockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionDeStockApplication.class, args);
+	}
+
+}
